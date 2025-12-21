361b475dbd6c81d37f6963f8f8cabcccf5471ba9
kevinchitsembe <151846246+kevinchitsembe@users.noreply.github.com>
2025-12-21 17:09:58 +0200

    Add logging utility in logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,1 +1,20 @@
 #BillionairForever
+import logging
+import sys
+
+def get_logger():
+    logger = logging.getLogger("retail_etl")
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
