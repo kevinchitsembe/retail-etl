d1bfa9b88e61c73216cec52343b0c1c4fb4f0c7d
kevinchitsembe <151846246+kevinchitsembe@users.noreply.github.com>
2025-12-21 16:23:25 +0200

    Implement KPI building and CSV export functionality
    Add function to build KPIs and save to CSV

--- app/transform/kpis.py
+++ app/transform/kpis.py
@@ -1,1 +1,20 @@
 #BillionairForever
+import os
+import pandas as pd
+from config.settings import TMP_OUTPUT
+
+def build_kpis(fact_sales: pd.DataFrame):
+    kpis = (
+        fact_sales
+        .groupby("Date")
+        .agg(
+            TotalSales=("Total", "sum"),
+            TotalQuantity=("Quantity", "sum")
+        )
+        .reset_index()
+    )
+
+    kpi_path = os.path.join(TMP_OUTPUT, "kpi_sales.csv")
+    kpis.to_csv(kpi_path, index=False)
+
+    return kpi_path
