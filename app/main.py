aae87643dfed29f9f0cdc6cc2097af8c6dac5abe
kevinchitsembe <151846246+kevinchitsembe@users.noreply.github.com>
2025-12-31 15:07:03 +0200

    Update header comment and add emojis to logs

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-#BillionairForever
+# BillionairForever 🚀
+
 from extract.google_drive import download_inputs
 from transform.etl import run_etl
 from load.onedrive import upload_outputs
 from utils.logger import get_logger
 
 logger = get_logger()
+
 
 def main():
     logger.info("🚀 ETL iniciado")
@@ -14,11 +16,13 @@ def main():
 
     logger.info("🔄 A transformar dados e criar KPIs")
     output_files = run_etl(input_files)
+    # output_files = ["fact_sales.xlsx", "dim_products.xlsx", "kpi_sales.xlsx"]
 
     logger.info("☁️ A carregar dados no OneDrive")
     upload_outputs(output_files)
 
     logger.info("✅ ETL finalizado com sucesso")
+
 
 if __name__ == "__main__":
     main()
